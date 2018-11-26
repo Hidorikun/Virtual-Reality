de0f6c10f16f254952c78237c1a6d53c5be144e9
Vele George <georgevele2016@gmail.com>
2018-11-26 23:44:14 +0200

    Add Cannons & Brick Wall

---
+++ AnimatingRobot/Assets/Scripts/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    public Rigidbody projectile;
+    public GameObject cannonHead; 
+    public Transform shotPosition;
+    public float shotForce = 4000f;
+    public float moveSpeed = 10f; 
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        
+        transform.Rotate(v, h, 0);
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            Rigidbody shot = Instantiate(projectile, shotPosition.position, shotPosition.rotation) as Rigidbody; 
+            shot.AddForce(shotPosition.forward * shotForce); 
+        }
+	}
+}
